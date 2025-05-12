6ec2b7e40d05fe41f90f1e36c1e6a9db1f8a20ed
wiimri <williams.arias.q@gmail.com>
2025-05-12 01:29:37 -0400

    subiendo assets completo

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,4 @@
+const example = function(a, b, c) {
+    return a + b + c;
+  };
+  
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const pintar = function(element, color = 'green') {
+  element.style.backgroundColor = color;
+};
+
+const ele = document.getElementById("ele1");
+ele.addEventListener("click", function () {
+  pintar(this, 'yellow');
+});
